e2724a076d396c8a2a6f6ba58eedc6eb03bfc982
Matheus Betim <mbetim47@gmail.com>
2024-03-10 15:59:33 -0300

    feat: add `Repeat` function with tests and benchmark

---
+++ iteration/repeat_test.go
@@ -0,0 +1,27 @@
+package iteration
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestRepeat(t *testing.T) {
+	repeated := Repeat("a", 3)
+	expected := "aaa"
+
+	if repeated != expected {
+		t.Errorf("expected %q but got %q", expected, repeated)
+	}
+}
+
+func BenchmarkRepeat(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Repeat("a", 5)
+	}
+}
+
+func ExampleRepeat() {
+	repeated := Repeat("a", 8)
+	fmt.Println(repeated)
+	// Output: aaaaaaaa
+}

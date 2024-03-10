7ff7d348ee5202d2b96e287e230d010cc1ec578f
Matheus Betim <mbetim47@gmail.com>
2024-03-10 20:07:12 -0300

    feat(structs): add shapes and their tests
    This commit introduces shapes (Rectangle, Circle, Triangle) in the `structs` package, along with their respective methods for calculating area and perimeter. It also includes tests for these methods.

---
+++ structs/shapes_test.go
@@ -0,0 +1,38 @@
+package structs
+
+import "testing"
+
+type Shape interface {
+	Area() float64
+}
+
+func TestPerimeter(t *testing.T) {
+	rectangle := Rectangle{10.0, 10.0}
+	got := rectangle.Perimeter()
+	want := 40.0
+
+	if got != want {
+		t.Errorf("got %.2f want %.2f", got, want)
+	}
+}
+
+func TestArea(t *testing.T) {
+	areaTests := []struct {
+		shape Shape
+		name  string
+		want  float64
+	}{
+		{name: "Rectangle", shape: Rectangle{Width: 12, Height: 6}, want: 72},
+		{name: "Circle", shape: Circle{10}, want: 314.1592653589793},
+		{name: "Triangle", shape: Triangle{12, 6}, want: 36.0},
+	}
+
+	for _, tt := range areaTests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := tt.shape.Area()
+			if got != tt.want {
+				t.Errorf("%v got %g want %g", tt.shape, got, tt.want)
+			}
+		})
+	}
+}
